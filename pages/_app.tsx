81af2f386176b09b98fdb4b91887e217b5788186
Sean Smith <101382476+LoftyyM@users.noreply.github.com>
2023-03-08 00:40:30 -0800

    Styling changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import '@component/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Encode_Sans } from '@next/font/google'
+
+const encode = Encode_Sans({
+  weight: "400",
+  subsets: ["latin"],
+  variable:"--encode-sans"
+}
+)
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} className={`${encode.variable} font-sans`}/>
 }
